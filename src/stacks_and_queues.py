d346fc6ee9cd8f970f38c27e4d5b200264e90f7d
Nicolás Pujia <nicopujia.dev@gmail.com>
2024-12-20 16:25:51 -0300

    Add Queue

--- src/stacks_and_queues.py
+++ src/stacks_and_queues.py
@@ -26,3 +26,34 @@ class Stack:
     def is_empty(self) -> bool:
         """Return true if and only if the stack is empty."""
         return not bool(self.top)
+
+
+class Queue:
+    first: LinkedListNode | None = None
+    last: LinkedListNode | None = None
+
+    def add(self, item: Any) -> None:
+        """Add an item to the end of the queue."""
+        if self.last:
+            self.last.next = LinkedListNode(item)
+            self.last = self.last.next
+        else:
+            self.first = self.last = LinkedListNode(item)
+
+    def remove(self) -> None:
+        """Remove the first item in the queue."""
+        if not self.first:
+            raise IndexError
+        self.first = self.first.next
+        if not self.first:
+            self.last = None
+
+    def peek(self) -> Any:
+        """Return the first of the queue."""
+        if not self.first:
+            raise IndexError
+        return self.first.data
+
+    def is_empty(self) -> bool:
+        """Return true if and only if the queue is empty."""
+        return not bool(self.first)
