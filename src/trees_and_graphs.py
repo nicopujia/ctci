c7826de17c38ed0bbf3ea5be63c3c647c95ac892
Nicolás Pujia <nicopujia.dev@gmail.com>
2024-12-23 16:23:29 -0300

    Implement Graph

--- src/trees_and_graphs.py
+++ src/trees_and_graphs.py
@@ -1,21 +1,64 @@
-from typing import Any, Iterable, Self
+from typing import Iterable
 
 
 class Node:
-    def __init__(self, data: Any):
+    def __init__(self, data):
         self.data = data
-        self.neighbors = set()
+        self._neighbors = set()
 
     def __repr__(self) -> str:
         return f"{self.data} -> " + ", ".join(
-            neighbor.data for neighbor in self.neighbors
+            neighbor.data for neighbor in self._neighbors
         )
 
-    def connect(self, other: Self) -> None:
-        self.neighbors.add(other)
+    @property
+    def neighbors(self):
+        return self._neighbors.copy()
 
-    def disconnect(self, other: Self) -> None:
-        self.neighbors.discard(other)
+    def add_neighbor(self, neighbor: "Node", graph: "Graph") -> None:
+        self._neighbors.add(neighbor)
+        graph.add(neighbor)
 
-    def is_connected(self, other: Self) -> bool:
-        return other in self.neighbors
+    def remove_neighbor(self, neighbor: "Node") -> None:
+        self._neighbors.discard(neighbor)
+
+    def points_to(self, other: "Node") -> bool:
+        return other in self._neighbors
+
+
+class Graph:
+    def __init__(self, nodes: Iterable[Node] = set()):
+        self._nodes = set(nodes)
+
+    def __repr__(self) -> str:
+        return f"Graph({len(self._nodes)} nodes)"
+
+    def __iter__(self):
+        for node in self._nodes:
+            yield node
+
+    def __in__(self, node: Node):
+        return node in self._nodes
+
+    def add(self, node: Node) -> None:
+        self._nodes.add(node)
+
+    def remove(self, node: Node) -> None:
+        self._nodes.discard(node)
+        for other_node in self._nodes:
+            self.disconnect(node, other_node, both_ways=True)
+
+    def connect(self, a: Node, b: Node, both_ways: bool = False) -> None:
+        a.add_neighbor(b, self)
+        if both_ways:
+            self.connect(b, a)
+
+    def disconnect(self, a: Node, b: Node, both_ways: bool = False) -> None:
+        a.remove_neighbor(b)
+        if both_ways:
+            self.disconnect(b, a)
+
+    def are_connected(self, a: Node, b: Node, both_ways: bool = False) -> bool:
+        if both_ways:
+            return a in b.neighbors and b in a.neighbors
+        return a in b.neighbors or b in a.neighbors
--- tests/test_trees_and_graphs.py
+++ tests/test_trees_and_graphs.py
@@ -1,143 +1,160 @@
-from src.trees_and_graphs import Node
+from src.trees_and_graphs import Graph, Node
 
 
-class TestNode:
+class TestGraph:
     def setup_method(self):
         self.n = [Node(i) for i in range(5)]
+        self.g = Graph(self.n)
 
     def test_nodes_data(self):
         for i in range(len(self.n)):
             assert self.n[i].data == i
 
     def test_no_neighbors_initially(self):
-        for i in range(5):
-            assert len(self.n[i].neighbors) == 0
+        for node in self.n:
+            assert len(node.neighbors) == 0
 
     def test_connect_one_to_itself(self):
-        self.n[0].connect(self.n[0])
-        assert self.n[0].is_connected(self.n[0])
+        self.g.connect(self.n[0], self.n[0])
+        assert self.g.are_connected(self.n[0], self.n[0])
 
     def test_connect_one_to_another_in_single_direction(self):
-        self.n[0].connect(self.n[1])
-        assert self.n[0].is_connected(self.n[1])
-        assert not self.n[1].is_connected(self.n[1])
+        self.g.connect(self.n[0], self.n[1])
+        assert self.n[0].points_to(self.n[1])
+        assert not self.n[1].points_to(self.n[0])
 
     def test_connect_same_nodes_multiple_times_keeps_one_connection(self):
-        self.n[0].connect(self.n[1])
-        self.n[0].connect(self.n[1])
+        self.g.connect(self.n[0], self.n[1])
+        self.g.connect(self.n[0], self.n[1])
         assert len(self.n[0].neighbors) == 1
 
     def test_connect_one_to_another_in_both_directions(self):
-        self.n[0].connect(self.n[1])
-        self.n[1].connect(self.n[0])
-        assert self.n[0].is_connected(self.n[1])
-        assert self.n[1].is_connected(self.n[0])
+        self.g.connect(self.n[0], self.n[1], both_ways=True)
+        assert self.g.are_connected(self.n[0], self.n[1], both_ways=True)
 
     def test_connect_one_to_many_single_direction(self):
         for i in range(1, 5):
-            self.n[0].connect(self.n[i])
+            self.g.connect(self.n[0], self.n[i])
         for i in range(1, 5):
-            assert self.n[0].is_connected(self.n[i])
-            assert not self.n[i].is_connected(self.n[0])
+            assert self.n[0].points_to(self.n[i])
+            assert not self.n[i].points_to(self.n[0])
 
     def test_connect_one_to_many_in_both_directions(self):
         for i in range(1, 5):
-            self.n[0].connect(self.n[i])
-            self.n[i].connect(self.n[0])
+            self.g.connect(self.n[0], self.n[i], both_ways=True)
         for i in range(1, 5):
-            assert self.n[0].is_connected(self.n[i])
-            assert self.n[i].is_connected(self.n[0])
+            assert self.g.are_connected(self.n[0], self.n[i], both_ways=True)
 
     def test_connect_one_to_many_and_one_of_them_to_the_other(self):
         for i in range(1, 4):
-            self.n[0].connect(self.n[i])
-        self.n[1].connect(self.n[2])
-        assert self.n[0].is_connected(self.n[1])
-        assert self.n[0].is_connected(self.n[2])
-        assert self.n[1].is_connected(self.n[2])
-        assert not self.n[2].is_connected(self.n[1])
+            self.g.connect(self.n[0], self.n[i])
+        self.g.connect(self.n[1], self.n[2])
+        assert self.n[0].points_to(self.n[1])
+        assert self.n[0].points_to(self.n[2])
+        assert self.n[1].points_to(self.n[2])
+        assert not self.n[2].points_to(self.n[1])
 
     def test_connect_many_to_one_single_direction(self):
         for i in range(1, 5):
-            self.n[i].connect(self.n[0])
+            self.g.connect(self.n[i], self.n[0])
         for i in range(1, 5):
-            assert self.n[i].is_connected(self.n[0])
-            assert not self.n[0].is_connected(self.n[i])
+            assert self.n[i].points_to(self.n[0])
+            assert not self.n[0].points_to(self.n[i])
 
     def test_connect_many_in_single_direction_with_cycle(self):
         for i in range(4):
-            self.n[i].connect(self.n[i + 1])
-        self.n[4].connect(self.n[0])
+            self.g.connect(self.n[i], self.n[i + 1])
+        self.g.connect(self.n[4], self.n[0])
         for i in range(4):
-            assert self.n[i].is_connected(self.n[i + 1])
-        assert self.n[4].is_connected(self.n[0])
+            assert self.n[i].points_to(self.n[i + 1])
+        assert self.n[4].points_to(self.n[0])
 
     def test_connect_many_in_single_direction_without_cycle(self):
         for i in range(4):
-            self.n[i].connect(self.n[i + 1])
+            self.g.connect(self.n[i], self.n[i + 1])
         for i in range(4):
-            assert self.n[i].is_connected(self.n[i + 1])
-        assert not self.n[4].is_connected(self.n[0])
+            assert self.n[i].points_to(self.n[i + 1])
+        assert not self.n[4].points_to(self.n[0])
 
     def test_connect_many_in_both_directions_with_cycle(self):
         for i in range(4):
-            self.n[i].connect(self.n[i + 1])
-            self.n[i + 1].connect(self.n[i])
-        self.n[4].connect(self.n[0])
-        self.n[0].connect(self.n[4])
+            self.g.connect(self.n[i], self.n[i + 1], both_ways=True)
+        self.g.connect(self.n[4], self.n[0], both_ways=True)
         for i in range(4):
-            assert self.n[i].is_connected(self.n[i + 1])
-            assert self.n[i + 1].is_connected(self.n[i])
-        assert self.n[4].is_connected(self.n[0])
-        assert self.n[0].is_connected(self.n[4])
+            assert self.g.are_connected(
+                self.n[i], self.n[i + 1], both_ways=True
+            )
+        assert self.g.are_connected(self.n[4], self.n[0], both_ways=True)
 
     def test_connect_many_in_both_directions_without_cycle(self):
         for i in range(4):
-            self.n[i].connect(self.n[i + 1])
-            self.n[i + 1].connect(self.n[i])
+            self.g.connect(self.n[i], self.n[i + 1], both_ways=True)
         for i in range(4):
-            assert self.n[i].is_connected(self.n[i + 1])
-            assert self.n[i + 1].is_connected(self.n[i])
-        assert not self.n[4].is_connected(self.n[0])
-        assert not self.n[0].is_connected(self.n[4])
+            assert self.g.are_connected(
+                self.n[i], self.n[i + 1], both_ways=True
+            )
+        assert not self.g.are_connected(self.n[4], self.n[0])
+        assert not self.g.are_connected(self.n[0], self.n[4])
 
     def test_connect_many_to_many_in_both_directions(self):
         for i in range(5):
             for j in range(5):
-                if j == i:
-                    continue
-                self.n[i].connect(self.n[j])
+                if i != j:
+                    self.g.connect(self.n[i], self.n[j], both_ways=True)
         for i in range(5):
             for j in range(5):
-                if j == i:
-                    assert not self.n[i].is_connected(self.n[j])
-                else:
-                    assert self.n[i].is_connected(self.n[j])
+                if i != j:
+                    assert self.g.are_connected(
+                        self.n[i], self.n[j], both_ways=True
+                    )
 
     def test_disconnect_disconnected_nodes_does_nothing(self):
-        assert not self.n[0].is_connected(self.n[1])
-        self.n[0].disconnect(self.n[1])
-        assert not self.n[0].is_connected(self.n[1])
+        assert not self.g.are_connected(self.n[0], self.n[1])
+        self.g.disconnect(self.n[0], self.n[1])
+        assert not self.g.are_connected(self.n[0], self.n[1])
+        self.g.disconnect(self.n[0], self.n[1], both_ways=True)
+        assert not self.g.are_connected(self.n[0], self.n[1])
 
     def test_disconnect_connected_nodes_in_single_direction(self):
-        self.n[0].connect(self.n[1])
-        assert self.n[0].is_connected(self.n[1])
-        assert not self.n[1].is_connected(self.n[0])
-        self.n[1].disconnect(self.n[0])
-        assert self.n[0].is_connected(self.n[1])
-        assert not self.n[1].is_connected(self.n[0])
-        self.n[0].disconnect(self.n[1])
-        assert not self.n[0].is_connected(self.n[1])
-        assert not self.n[1].is_connected(self.n[0])
+        self.g.connect(self.n[0], self.n[1])
+        assert self.g.are_connected(self.n[0], self.n[1])
+        self.g.disconnect(self.n[1], self.n[0])
+        assert self.g.are_connected(self.n[0], self.n[1])
+        self.g.disconnect(self.n[0], self.n[1])
+        assert not self.g.are_connected(self.n[0], self.n[1])
 
-    def test_disconnect_connected_nodes_in_both_direction(self):
-        self.n[0].connect(self.n[1])
-        self.n[1].connect(self.n[0])
-        assert self.n[0].is_connected(self.n[1])
-        assert self.n[1].is_connected(self.n[0])
-        self.n[0].disconnect(self.n[1])
-        assert not self.n[0].is_connected(self.n[1])
-        assert self.n[1].is_connected(self.n[0])
-        self.n[1].disconnect(self.n[0])
-        assert not self.n[0].is_connected(self.n[1])
-        assert not self.n[1].is_connected(self.n[0])
+    def test_disconnect_connected_nodes_in_both_directions(self):
+        self.g.connect(self.n[0], self.n[1], both_ways=True)
+        self.g.disconnect(self.n[1], self.n[0], both_ways=True)
+        assert not self.g.are_connected(self.n[0], self.n[1])
+        self.g.connect(self.n[0], self.n[1], both_ways=True)
+        self.g.disconnect(self.n[0], self.n[1], both_ways=True)
+        assert not self.g.are_connected(self.n[0], self.n[1])
+
+    def test_remove_node_without_connections(self):
+        self.g.remove(self.n[0])
+        assert self.n[0] not in self.g
+
+    def test_remove_node_with_connections(self):
+        for i in range(2):
+            self.g.connect(self.n[i], self.n[i + 1])
+        self.g.remove(self.n[1])
+        assert self.n[0] in self.g
+        assert self.n[1] not in self.g
+        assert self.n[2] in self.g
+        for i in range(3):
+            assert not self.n[i].neighbors
+
+    def test_remove_node_with_connections_from_two_graphs(self):
+        self.g.connect(self.n[0], self.n[1], both_ways=True)
+        g2_node = Node(2)
+        g2 = Graph([self.n[1], g2_node])
+        g2.connect(self.n[1], g2_node, both_ways=True)
+        # At this point we have two graphs like this: [n0 <-> (n1] <-> g2_node)
+        self.g.remove(self.n[1])
+        assert g2_node in g2
+        assert self.n[1] in g2
+        assert self.n[1] not in self.g
+        assert self.n[0] in self.g
+        assert g2.are_connected(self.n[1], g2_node, both_ways=True)
+        assert not self.g.are_connected(self.n[0], self.n[1])
